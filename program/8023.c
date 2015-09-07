a8aad2643093a76b9f30976e3668ad27fb1729e9
8023 <lxy_05@126.com>
2015-09-07 16:49:59 +0800

    1.优化了初始化 2.修复了若干小细节
    Signed-off-by: 8023 <lxy_05@126.com>

--- program/8023.c
+++ program/8023.c
@@ -102,36 +102,6 @@ void fun_wait(){
     while(in_start==0);
     fun_delay(256,del_ms);
 }//等待按键
-void fun_initialization(){
-    CLK_DIV=0x00;//不分频
-
-    P0M1=0xff;//P0用于输入
-    P0M0=0x00;//P0不能输出
-    
-    P1M1=0x00;//P1口0-1双向，2中断输入，3-6输入，7输出
-    P1M0=0xfc;//P1口2-7输出
-
-    P2M1=0xf0;//P2口4-7输入
-    P2M0=0x0f;//P2口0-3输出
-
-    //PS_2=1;//手抓松传感器置1 $?$
-    //PS_11=1;//升降位置3传感器置1 $?$
-
-    out_en1=1;//电机1/3使能 $?$
-    out_en2=1;//电机2/4使能 $?$
-    //out_motorselect=1;//电机片选为1 $?$
-    //fun_delay(del_ms,1);//延时1毫秒 $?$
-    out_motorselect=0;//电机片选为0 $?$
-    //PS_5=PS_1;// $?$
-
-    fun_pwminit();//PWM的初始化设置
-    fun_timer0init();//初始化定时器0
-    fun_timer1init();//初始化定时器1
-    fun_port();//初始化串口
-    in_start=1;//按键置1
-
-    fun_wait();
-}//初始化
 void fun_pwminit(){
     CCON=0x00;//PAC寄存控制器
     CH=0;//重置PAC计时器
